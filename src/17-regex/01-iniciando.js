f8469459b8368ca16cb75924c37ab0b5dddb045e
Nilton Lopes <niltonrck@gmail.com>
2019-04-13 20:44:09 -0300

    Testando alguns caracteres especias utilizados para realizar uma busca

--- src/17-regex/01-iniciando.js
+++ src/17-regex/01-iniciando.js
@@ -9,7 +9,30 @@
  * Exemplo 01 - Utilizando expressões da forma literal:
  */
 
-var reg1 = /ab+c/
-var str1 = 'abbc'
+// \ => Pesquisando por um caractere literal
 
-console.log(str1.match(reg1))
+let str = 'Looking for something.'
+console.log(str.match(/\./g))
+
+// ^x => Pesquisando um caractere no inicio da string
+// i => Não case sentive
+console.log(str.match(/^l/gi))
+
+// x$ => Pesquisando um caractere no final da string
+console.log(str.match(/\.$/g))
+
+// x* => Pesquisando por 0 ou mais caractres em uma string
+str = 'Bananaaana'
+console.log(str.match(/na*/g))
+
+// (x)* => Pesquisando por 0 ou mais grupo de caractres em uma string
+console.log(str.match(/(na)*/g))
+
+// x+ => Pesquisando por 1 ou mais caracteres em uma string
+console.log(str.match(/na+/g))
+
+// (x)+ Pesquisando por 1 ou mais grupo de caracteres em uma string
+console.log(str.match(/(na)+/g))
+
+// x? => Pesquisando por 0 ou 1 vez o caracter precedido em uma string
+console.log(str.match(/a?/g))
